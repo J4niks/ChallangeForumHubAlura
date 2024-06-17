b136eea4c59f0629ce8fa80a50ccbda3fe5c02eb
Janiks Oliveira <janiksoliveira22@gmail.com>
2024-06-17 08:40:15 -0300

    feat: first commit

---
+++ src/main/java/com/janiks/forumHub/ForumHubApplication.java
@@ -0,0 +1,13 @@
+package com.janiks.forumHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumHubApplication.class, args);
+	}
+
+}
