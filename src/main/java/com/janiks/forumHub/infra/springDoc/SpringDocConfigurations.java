8b90251c079ef471680fffaa5e83a2a8a2427361
Janiks Oliveira <janiksoliveira22@gmail.com>
2024-06-18 07:55:07 -0300

    feat: Documentation with spring doc

---
+++ src/main/java/com/janiks/forumHub/infra/springDoc/SpringDocConfigurations.java
@@ -0,0 +1,33 @@
+package com.janiks.forumHub.infra.springDoc;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfigurations {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .info(new Info()
+                        .title("Forum Hub")
+                        .description("API Rest da aplicação Voll.med, contendo as funcionalidades de CRUD de médicos e de pacientes, além de agendamento e cancelamento de consultas")
+                        .contact(new Contact()
+                                .name("creator")
+                                .email("contato.janiksoliveira@gmail.com"))
+                        .license(new License()
+                                .name("MIT")
+                                .url("https://github.com/J4niks/ChallangeForumHubAlura/blob/master/LICENSE")));
+    }
+ 
+
+}
