714689686ccbb911977174d3d3d4474efd76ebd0
Janiks Oliveira <janiksoliveira22@gmail.com>
2024-06-19 10:05:03 -0300

    edit: security configs for /cursos

--- src/main/java/com/janiks/forumHub/infra/security/SecurityConfigurations.java
+++ src/main/java/com/janiks/forumHub/infra/security/SecurityConfigurations.java
@@ -3,6 +3,7 @@ package com.janiks.forumHub.infra.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -25,6 +26,9 @@ public class SecurityConfigurations {
         return http.csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(req -> {
+                    req.requestMatchers(HttpMethod.POST, "/cursos").hasRole("ADMIN");
+                    req.requestMatchers(HttpMethod.DELETE, "/cursos").hasRole("ADMIN");
+                    req.requestMatchers(HttpMethod.PUT, "/cursos").hasRole("ADMIN");
                     req.requestMatchers("/login", "/user").permitAll();
                     req.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll();
                     req.anyRequest().authenticated();
